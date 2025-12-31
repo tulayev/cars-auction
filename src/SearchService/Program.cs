2e9a135650110234e0356a724110a41ff44615ec
Umarbek Tulayev <tumarbekttt@gmail.com>
2026-01-01 01:38:19 +0500

    feat: add SearchService with MongoDB integration
    Introduces a new SearchService project to the solution, including controllers, models, data initialization, and HTTP client integration for syncing auction items. Updates docker-compose to add a MongoDB service and adjusts AuctionService's database password for consistency. The solution and configuration files are updated to support the new service.

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,40 @@
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Services;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>()
+    .AddPolicyHandler(GetRetryPolicy());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error initializing database: {ex.Message}");
+    }
+});
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
+}
